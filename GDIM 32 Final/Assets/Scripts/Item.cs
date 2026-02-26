e0a2620872805f3856b4d0699bd645e0fbac8e8b
AlePerez25 <alejp12@uci.edu>
2026-02-25 22:27:56 -0800

    Click flower

---
+++ GDIM 32 Final/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObjects/Item")]
+
+
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public string description;
+    public Sprite icon;
+}
+
