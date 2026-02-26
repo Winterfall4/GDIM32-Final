770793240a489872a5a83b31455935bc36b1af82
Winterfal4444 <ugianine143@gmail.com>
2026-02-26 13:10:14 -0800

    NPC Class and NPCState enums made

--- GDIM 32 Final/Assets/Scripts/NPC.cs
+++ GDIM 32 Final/Assets/Scripts/NPC.cs
@@ -3,7 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum NPCState
+{
+    isIdle,
+    isTalking
+}
 public class NPC : MonoBehaviour
 {
-   
+
+    public NPCState _currentState;
+    public float _talkingDistance;
+    public float _distance;
+    [SerializeField] public GameObject _player;
+
+
+    public void CheckDistance()
+    {
+        _distance = (transform.position - _player.transform.position).sqrMagnitude;
+
+        if (_distance < _talkingDistance)
+        {
+            _currentState = NPCState.isTalking;
+        }
+
+        else
+        {
+            _currentState = NPCState.isIdle;
+        }
+    }
 }
