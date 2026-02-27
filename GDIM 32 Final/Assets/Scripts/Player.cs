92741ddc0aea43561e597e5004d4f9dbb1fdebf2
kxcastilliano <kxcastil@uci.edu>
2026-02-27 15:11:02 -0800

    Fixed Camera/Cursor lock for player

--- GDIM 32 Final/Assets/Scripts/Player.cs
+++ GDIM 32 Final/Assets/Scripts/Player.cs
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
         _cameraTrans = Camera.main.transform;
     }
 
